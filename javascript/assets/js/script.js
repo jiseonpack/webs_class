25391c6bdd600d4c17f664b63006c624fbcf5051
jisun <jsjh442@gmail.com>
2022-01-17 10:07:13 +0900

    22/01/17

---
+++ javascript/assets/js/script.js
@@ -0,0 +1,85 @@
+//결과보기
+//제목을 클릭했을 때 ~~~~ 실행하라 !
+document.querySelector(".result01t h4").addEventListener("click", function(){
+    document.querySelector(".result01").style.display = "block";
+})
+
+document.querySelector(".result02t h4").addEventListener("click", function(){
+    document.querySelector(".result02").style.display = "block";
+})
+
+document.querySelector(".result03t h4").addEventListener("click", function(){
+    document.querySelector(".result03").style.display = "block";
+})
+
+document.querySelector(".result04t h4").addEventListener("click", function(){
+    document.querySelector(".result04").style.display = "block";
+})
+
+document.querySelector(".result05t h4").addEventListener("click", function(){
+    document.querySelector(".result05").style.display = "block";
+})
+
+document.querySelector(".result06t h4").addEventListener("click", function(){
+    document.querySelector(".result06").style.display = "block";
+})
+
+document.querySelector(".result07t h4").addEventListener("click", function(){
+    document.querySelector(".result07").style.display = "block";
+})
+
+document.querySelector(".result08t h4").addEventListener("click", function(){
+    document.querySelector(".result08").style.display = "block";
+})
+
+document.querySelector(".result09t h4").addEventListener("click", function(){
+    document.querySelector(".result09").style.display = "block";
+})
+
+document.querySelector(".result10t h4").addEventListener("click", function(){
+    document.querySelector(".result10").style.display = "block";
+})
+
+document.querySelector(".result11t h4").addEventListener("click", function(){
+    document.querySelector(".result11").style.display = "block";
+})
+
+document.querySelector(".result12t h4").addEventListener("click", function(){
+    document.querySelector(".result12").style.display = "block";
+})
+
+document.querySelector(".result13t h4").addEventListener("click", function(){
+    document.querySelector(".result13").style.display = "block";
+})
+
+document.querySelector(".result14t h4").addEventListener("click", function(){
+    document.querySelector(".result14").style.display = "block";
+})
+
+document.querySelector(".result15t h4").addEventListener("click", function(){
+    document.querySelector(".result15").style.display = "block";
+})
+
+document.querySelector(".result16t h4").addEventListener("click", function(){
+    document.querySelector(".result16").style.display = "block";
+})
+
+document.querySelector(".result17t h4").addEventListener("click", function(){
+    document.querySelector(".result17").style.display = "block";
+})
+
+document.querySelector(".result18t h4").addEventListener("click", function(){
+    document.querySelector(".result18").style.display = "block";
+})
+
+document.querySelector(".result19t h4").addEventListener("click", function(){
+    document.querySelector(".result19").style.display = "block";
+})
+
+document.querySelector(".result20t h4").addEventListener("click", function(){
+    document.querySelector(".result20").style.display = "block";
+})
+
+document.querySelector(".result21t h4").addEventListener("click", function(){
+    document.querySelector(".result21").style.display = "block";
+})
