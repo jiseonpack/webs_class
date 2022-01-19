fe183994444122e98eacf4528bfed410849d51aa
jisun <jsjh442@gmail.com>
2022-01-19 11:55:18 +0900

    22/01/19

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,6 +1,6 @@
 //결과보기 버튼을 클릭하면 경고창을 띄어주세요~
 document.querySelectorAll(".result").forEach(function(el){
     el.addEventListener("click", function(){
-        alert("dd")
+        el.classList.toggle("active");
     });
 });
