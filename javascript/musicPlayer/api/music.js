1515e915de4933a8891ab5f497a8804d0b73e5e6
jisun <97070112+jiseonpack@users.noreply.github.com>
2022-04-07 14:55:30 +0900

    22/04/07

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,58 @@
+
+    let allMusic = [
+        {
+            name : "Mekanın Sahibi01",
+            artist : "Norm Ender" ,
+            img : "music-1" ,
+            audio : "music-1" 
+        },
+        {
+            name : "Mekanın Sahibi02",
+            artist : "Norm Ender" ,
+            img : "music-2" ,
+            audio : "music-2" 
+        },
+        {
+            name : "Mekanın Sahibi03",
+            artist : "Norm Ender" ,
+            img : "music-3" ,
+            audio : "music-3" 
+        },
+        {
+            name : "Mekanın Sahibi04",
+            artist : "Norm Ender" ,
+            img : "music-4" ,
+            audio : "music-4" 
+        },
+        {
+            name : "Mekanın Sahibi05",
+            artist : "Norm Ender" ,
+            img : "music-5" ,
+            audio : "music-5" 
+        },
+        {
+            name : "Mekanın Sahibi06",
+            artist : "Norm Ender" ,
+            img : "music-6" ,
+            audio : "music-6" 
+        },
+        {
+            name : "Mekanın Sahibi07",
+            artist : "Norm Ender" ,
+            img : "music-7" ,
+            audio : "music-7" 
+        },
+        {
+            name : "Mekanın Sahibi08",
+            artist : "Norm Ender" ,
+            img : "music-8" ,
+            audio : "music-8" 
+        },
+        {
+            name : "Mekanın Sahibi09",
+            artist : "Norm Ender" ,
+            img : "music-9" ,
+            audio : "music-9" 
+        },
+    ]
+   
